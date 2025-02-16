e08348ea53887926cb718b90e03f9ff14b790e27
Ak-Yadav3122 <aichchhik12@gmail.com>
2025-02-16 17:26:32 +0530

    added sidebar for  dashboard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import BlogDashboard from '@/components/dashboard/blog-dashboard'
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <BlogDashboard/>
+  )
+}
+
+export default Dashboard
