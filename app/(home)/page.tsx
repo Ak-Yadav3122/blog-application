9c553ce6b1ec9784012f618aea3a691396045e52
Ak-Yadav3122 <aichchhik12@gmail.com>
2025-02-17 16:45:45 +0530

    Update  the CSS for responsive look

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,25 +4,27 @@ import { TopArticles } from "@/components/home/top-articles";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { Suspense } from "react";
+import { AllArticlesPageSkeleton } from "../articles/page";
 
 const page = async () => {
   return (
     <main>
       <HeroSection />
-      <section className="relative py-16 md:py-24">
+      <section className="relative w-full overflow-hidden bg-gradient-to-br from-gray-950 via-indigo-950 to-indigo-950  py-16 md:py-24">
         <div className="container mx-auto px-4">
           <div className="mb-12 text-center">
-            <h2 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-4xl">
-              Featured Articles
+            <h2 className="text-3xl font-bold relative tracking-tight text-indigo-700 dark:text-indigo-200 transition-colors duration-500 ease-in-out sm:text-4xl">
+            Featured Articles
             </h2>
-            <p className="mt-4 text-lg text-gray-600 dark:text-gray-300">
+
+            <p className="mt-4 text-lg text-pink-500 dark:text-gray-300">
               Discover our most popular and trending content
             </p>
           </div>
 
           {/* Top Articles */}
-          <Suspense fallback={<h1>Loading....</h1>}>
-            <TopArticles/>
+          <Suspense fallback={<AllArticlesPageSkeleton />}>
+            <TopArticles />
           </Suspense>
 
           <div className="mt-12 text-center">
