ea3af9d5c3c903235eedd1dd9cda8934535f94c8
Ak-Yadav3122 <aichchhik12@gmail.com>
2025-02-18 21:30:46 +0530

    adding tutorial route page

---
+++ app/tutorials/page.tsx
@@ -0,0 +1,10 @@
+import TutorialPage from '@/components/tutorial/tutorial-section'
+import React from 'react'
+
+const Tutorials = () => {
+  return (
+    <TutorialPage/>
+  )
+}
+
+export default Tutorials
